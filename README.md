26cfa0a16ca23ab2363609d1e7088ea7b9aa6fdb
Shikhar Vashistha <shikharvashistha@yandex.com>
2022-01-18 10:03:56 +0000

    Completed

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # bug-tracking
+
+### Setup Docker
+
+sudo docker run --name postgresSQL -e POSTGRES_PASSWORD=123 -p 5432:5432 -d postgres
+docker exec -it [container_id] bash
+psql -U postgres //connect to database
+\l //list databases
+\c postgres //connect to database
+docker logs -f [container_id] //to get logs in seperate cli
+sudo docker ps --all //list all containers
+
+Day 4 2nd half
+15-30 minutes slides
+
+
+python3 -m pip install --upgrade pip
+pip install "uvicorn[standard]"
+pip install fastapi
+pip install psycopg2
+uvicorn main:app --reload
+
+CREATE TABLE table_name(
+  bug_id int,
+  priority int,
+  type varchar(10),
+  posted_by char(10),
+  assigned_to char(10),
+  status char(10),
+  summary char(50),
+  description char(50),
+   PRIMARY KEY( bug_id )
+);
+
+
+INSERT INTO bugs(bug_id, priority, type, posted_by, assigned_to, status, description)
+VALUES (2, 3, 'help', 'shikhar', 'shikhar', 'closed', 'help request');
+
+http://127.0.0.1:8000/bug/create/%7B1%7D/%7B3%7D/%7B%22support%22%7D/%7B%22shikharvashistha%22%7D/%7B%22shikharvashistha%22%7D/%7B%22opened%22%7D/%7B%22supportreques%22%7D
+
+
+/bug/create/{1}/{3}/{"support"}/{"shikhar"}/{"shikhar"}/{"opened"}/{"support request"}
+
+github.com/login?
+
+http://127.0.0.1:8000/bug/create?bug_id=1&priority=3&type=issue&posted_by=shikhar&assigned_to=shikhar&status=opened&description=issue
---
+++ src/main.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+
+import psycopg2
+
+from config import config
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def init():
+    return {"Bug": "Tracking"}
+
+
+@app.get("/bug/create/{bug_id}/{priority}/{type}/{posted_by}/{assigned_to}/{status}/{description}")
+def create(bug_id: int, priority: int, type: str, posted_by: str, assigned_to: str, status: str, summary: str, description: str):
+    params = config()
+    conn = psycopg2.connect(**params)
+    print("Connected to database")
+    cur = conn.cursor()
+    cur.execute("INSERT INTO bugs (bug_id, priority, type, posted_by, assigned_to, status, summary, description) VALUES (%s, %s, %s, %s, %s, %s, %s, %s);", (bug_id, priority, type, posted_by, assigned_to, status, summary, description))
+    conn.commit()
+    cur.close()
+    conn.close()
+    return {"Bug": "Created"}
+
+@app.get("/bug/update/{bug_id}/{priority}/{type}/{posted_by}/{assigned_to}/{status}/{description}")
+def update(bug_id: int, priority: int, type: str, posted_by: str, assigned_to: str, status: str, summary: str, description: str):
+    params = config()
+    conn = psycopg2.connect(**params)
+    print("Connected to database")
+    cur = conn.cursor()
+    cur.execute("UPDATE bugs SET priority = %s, type = %s, posted_by = %s, assigned_to = %s, status = %s, summary = %s, description = %s WHERE bug_id = %s;", (priority, type, posted_by, assigned_to, status, summary, description, bug_id))
+    conn.commit()
+    cur.close()
+    conn.close()
+    return {"Bug": "Updated"}
+
+@app.delete("/bug/delete/{bug_id}")
+def delete(bug_id: int):
+    params = config()
+    conn = psycopg2.connect(**params)
+    cur = conn.cursor()
+    print("Connected to database")
+    cur.execute("DELETE FROM bugs WHERE bug_id = %s;", (bug_id,))
+    conn.commit()
+    cur.close()
+    conn.close()
+    return {"Bug ": "Deleted"}
+
+@app.get("/bug/list/")
+def list():
+    params = config()
+    conn = psycopg2.connect(**params)
+    cur = conn.cursor()
+    print("Connected to database")
+    cur.execute("SELECT * FROM bugs;")
+    rows = cur.fetchall()
+    cur.close()
+    conn.close()
+    return rows
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+	main()
